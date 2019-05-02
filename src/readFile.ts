d5804b365cd1c6282646581dbcf044418c71cff7
Lilith Tundrus <echofox123@gmail.com>
2019-05-02 19:45:44 -0400

    Created a module to read text from a file

---
+++ src/readFile.ts
@@ -0,0 +1,30 @@
+// Using ES6 strict mode (not 100% needed, but ensures that the compiled JS is in strict mode)
+'use strict';
+
+// Node dependencies
+import * as fs from 'fs';
+import * as path from 'path';
+
+
+export function readFile(path: string): Buffer | void {
+
+    // First, make sure the path exists
+    if (!fs.existsSync(path)) {
+        console.log(`\nFile ${path} does not exist.`);
+        return;
+    }
+
+    // Variable to hold the contents of the file being read and used throughout the classs
+    let contents: Buffer;
+
+    // Try and read the file
+    try {
+        contents = fs.readFileSync(path);
+        return contents;
+    }
+    // Else, print an error that the file cannot be opened after starting the editor
+    catch (err) {
+        console.log(`\nCould not read file ${path}: ${err}`);
+        return;
+    }
+}
