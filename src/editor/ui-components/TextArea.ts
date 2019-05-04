74723f6a3af15b5f7975e5f370e79d4864b8e12a
Lilith Tundrus <echofox123@gmail.com>
2019-05-04 05:52:39 -0400

    Working on getting some of the textarea basics in place for testing

--- src/editor/ui-components/TextArea.ts
+++ src/editor/ui-components/TextArea.ts
@@ -1,3 +1,141 @@
 // Using ES6 strict mode (not 100% needed, but ensures that the compiled JS is in strict mode)
 'use strict';
 
+// NPM dependencies
+import * as blessed from 'blessed';
+// Local dependencies
+import Editor from '../Editor';
+// Used for debugging
+import * as fs from 'fs';
+
+// This file contains one of the blessed components for constructing the UI in an effort to
+// keep this project modular
+
+// Create the textArea textbox, where the text being edited will be displayed
+
+export default class TextArea {
+
+    private editorInstance: Editor;
+
+    textArea: blessed.Widgets.BoxElement;
+
+
+    constructor(editorInstance: Editor) {
+        this.editorInstance = editorInstance;
+
+        // Create the textArea blessed box (declared as any due to some typings being incorrect)
+        this.textArea = blessed.box(<any>{
+            // Parent option for the component, controls how the element interacts with others
+            parent: this.editorInstance.screen,
+
+            // Component relative position options
+
+            // The top of this element should be the parent width plus 1
+            top: 1,
+
+
+            // Component size options
+
+            // Keep the width of this element to 100% of the screen
+            width: '100%+1',
+            // Height should be the entire screen minus 1 because of the statusBar 
+            // (not doing this would hide part of the text entry window)
+            height: '100%-1',
+
+
+            // Key related options
+
+            // Allow input of the element
+            input: true,
+            // Dissallow default key mappings
+            keys: false,
+            // Set the element to support all key inputs
+            keyable: true,
+
+
+            // Content control options
+
+            // Don't capture SGR blessed escape codes, that could cause issues
+            tags: false,
+            // Don't shrink the text box if the window resizes
+            shrink: false,
+            // Dissallow text to wrap down the the next line (not documented but still works)
+            wrap: false,
+            visible: true,
+
+
+            // Alignment options
+
+            // Left align the text for this element
+            align: 'left',
+
+
+            // Scrolling options
+
+            // Allow the element to be scrollable
+            scrollable: true,
+            // Always allow the element to be scrollable, even if the content is shorter
+            // than the height of the windows
+            alwaysScroll: true,
+            // Scrollbar styles, using extended characters here to 
+            // represent the scroll location character
+            scrollbar: {
+                ch: '█',
+                track: {
+                    bg: 'black',
+                    ch: '░'
+                },
+            },
+            // Limit the maximum content to 16,000 lines (at least initially)
+            baseLimit: 16000,
+
+
+            // Border options
+
+            border: {
+                type: 'line'
+            },
+
+
+            // Styling options
+
+            // This style matches the DOS edit theme
+            style: {
+                fg: 'bold',
+                bg: 'blue',
+                border: {
+                    fg: 'light-grey',
+                },
+                label: {
+                    fg: 'black',
+                    bg: 'light-grey'
+                }
+            },
+
+            // Content/label options
+
+            // The label is a string that sits on the top left corner of the element,
+            // this is similar to a title windows
+            label: this.editorInstance.getRelativePath(),
+
+            content: this.editorInstance.getContent()
+        });
+
+        // Quit on Control-W
+        // TODO: This should be aware of whether or not the editor has a file that isn't saved/etc.
+        this.textArea.key(['C-w'], () => {
+            return process.exit(0);
+        });
+
+        this.textArea.key('left', () => {
+
+        });
+
+        this.textArea.key('right', () => {
+            this.textArea.position.left = this.textArea.position.left -1;
+            this.editorInstance.screen.render();
+        });
+    }
+
+
+}
