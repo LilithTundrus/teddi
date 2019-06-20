f5f7a0772ea3d04968853650bc91c61fdeac99d3
Lilith Tundrus <echofox123@gmail.com>
2019-06-20 01:52:18 -0400

    Added a PH file for the FileMenu UI element

---
+++ src/editor/ui-components/FileMenu.ts
@@ -0,0 +1,9 @@
+// Using ES6 strict mode (not 100% needed, but ensures that the compiled JS is in strict mode)
+'use strict';
+
+// NPM dependencies
+import * as blessed from 'blessed';
+// Local dependencies
+import Editor from '../Editor';
+
+// This file will hold methods for the FILE menubar item
