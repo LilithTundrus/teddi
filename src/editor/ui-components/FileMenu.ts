2de25cdaf050d4b77ee5c83e03a11b8284427dfd
Lilith Tundrus <echofox123@gmail.com>
2019-07-27 19:59:07 -0400

    Added basics for the File Menu UI element (not yet functional)

--- src/editor/ui-components/FileMenu.ts
+++ src/editor/ui-components/FileMenu.ts
@@ -7,3 +7,84 @@ import * as blessed from 'blessed';
 import Editor from '../Editor';
 
 // This file will hold methods for the FILE menubar item
+
+export default class FileMenu {
+
+    private editorInstance: Editor;
+
+    fileMenu: blessed.Widgets.BoxElement;
+
+    menuList: any;
+
+    constructor() {
+        // Create the fileMenuBox
+        this.fileMenu = blessed.box({
+            parent: this.editorInstance.screen,
+            // Top of this element should be 1 below the top of the screen
+            top: 1,
+            // Left start of this element should be 1 to the left of the screen for padding/looks
+            left: 1,
+            // Fixed width because the box's contents will be static
+            width: 15,
+            // Fixed height because the box's contents will be static
+            height: 7,
+            style: {
+                fg: 'black',
+                bg: 'lightgrey',
+                border: {
+                    fg: 'black',
+                    bg: 'lightgrey'
+                }
+            },
+            shadow: true
+        });
+
+
+        this.menuList = blessed.list(<any>{
+            // Borders to match how DOS Edit has borders for its menus
+            border: 'line',
+            padding: 'none',
+            // The parent of the titlebar should be the generated fileMenu
+            parent: this.fileMenu,
+            // Width and height should be the same as the parent
+            width: this.fileMenu.width,
+            height: this.fileMenu.height,
+            // Use the default keys for the belssed fileMenu
+            keys: true,
+            style: {
+                border: {
+                    fg: 'black',
+                    bg: 'lightgrey'
+                },
+                // Style of the currently selected list item
+                selected: {
+                    fg: 'lightgrey',
+                    bg: 'black'
+                },
+                // Style of any other listItem
+                item: {
+                    fg: 'black',
+                    bg: 'lightgrey'
+                }
+            }
+        });
+
+        // Set the items for the menu list
+        this.menuList.setItems(['New', 'Open...', 'Save', 'Save As...', 'Exit']);
+
+        // Append each UI subcomponent to the fileMenu box
+        this.fileMenu.append(this.menuList);
+
+        this.menuList.key(['escape'], () => {
+            // This is temporary, it needs to focus the last focused element before the menu
+            this.editorInstance.textArea.textArea.focus();
+        });
+
+        this.menuList.on('select', (item) => {
+            // The text parse is item.content
+
+            // TODO: This needs to make sure the file is properly saved before exiting
+            if (item.content == 'Exit') return process.exit();
+        });
+    }
+}
